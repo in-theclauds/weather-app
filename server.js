17c81f49a42aca9d34009d7ee8e7c2514b8df228
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-07-23 22:06:34 -0400

    api returned in json format and farenheit

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const request = require('request');
 
 let apiKey = 'ba2b8c20c3ea596f3b8d2a56dca6b2c9';
 let city = 'miami beach';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+
 
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
 
